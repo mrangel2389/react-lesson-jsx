1a9ab8808d2bc76f35e48849650d7e16dcda8de8
Michael Rangel <Mrangel@nexient.com>
2019-09-09 10:22:50 -0400

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+	return <div> Hi There!</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.getElementById('root'));
